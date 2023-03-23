5a9a0bcedee981d8a3e0ae5a84be4b7845ad872c
SabrinaMAlves <114673380+SabrinaMAlves@users.noreply.github.com>
2023-03-23 08:55:55 -0300

    atualização2

--- Cadastro.cs
+++ Cadastro.cs
@@ -11,21 +11,25 @@ using System.Windows.Forms;
 //using FirebaseAdmin;
 //using FirebaseAdmin.Auth;
 //using Google.Apis.Auth.OAuth2;
+// Utilizãção para autenticação 
+using Firebase.Auth;
+using Firebase.Auth.Providers;
+using Firebase.Auth.Repository;
 
 namespace CriandoMinhaAutenticacao
 {
     public partial class Cadastro : Form
     {
-        FirebaseAuth auth = FirebaseAuth.DefaultInstance; // instalação de pacote do firabase 
+        //FirebaseAuth auth = FirebaseAuth.DefaultInstance; // instalação de pacote do firabase 
 
 
         public Cadastro()
         {
             InitializeComponent();
-            FirebaseApp.Create(new AppOptions()
+            /*FirebaseApp.Create(new AppOptions()
             {
                 Credential = GoogleCredential.FromFile("auth.json") // caminho do arquivo JSON
-            });
+            });*/
         }
 
         private void BtnCancelar_Click(object sender, EventArgs e) // retorna para login 
@@ -52,21 +56,29 @@ namespace CriandoMinhaAutenticacao
                 MessageBox.Show("Digite uma senha com no minimo 8 caracteres");
                 return;
             }
+
             try
             {
-
-                var usuario = FirebaseAuth.DefaultInstance.CreateUserAsync(new UserRecordArgs()
+                var config = new FirebaseAuthConfig
                 {
-                    Email = TxtEmail.Text,
-                    Password = TxtSenha.Text,
-                }).Result;
-                MessageBox.Show(usuario.Uid);
-                MessageBox.Show(usuario.EmailVerified.ToString());
-                 Principal principal = new Principal();
-                 Hide();
-                 principal.Closed += fecharFormulario;
-                 principal.Show(); 
+                    ApiKey = "AIzaSyD7LUHpfYpYRNEP32olS3m-TWzU_y3yM6U",
 
+                    AuthDomain = "aula-09-03-2023-138a2.firebaseapp.com",
+                    Providers = new FirebaseAuthProvider[]
+                {
+                    new EmailProvider()
+                },
+                    UserRepository = new FileUserRepository
+                ("NossosDados")
+                };
+                var Cliente = new FirebaseAuthClient(config);
+                Cliente.CreateUserWithEmailAndPasswordAsync
+                (TxtEmail.Text, TxtSenha.Text);
+                Thread.Sleep(1000);
+                Principal principal = new Principal();
+                Hide();
+                principal.Closed += fecharFormulario;
+                principal.Show();
             }
             catch (Exception ex)
             {
@@ -78,10 +90,41 @@ namespace CriandoMinhaAutenticacao
                 {
 
                     MessageBox.Show("Erro:" + ex.Message);
-                   //  Clipboard.SetText(ex.Message); //
+
                 }
+
+                //Versão oficial do google 
+                /*try
+                {
+
+                    var usuario = FirebaseAuth.DefaultInstance.CreateUserAsync(new UserRecordArgs()
+                    {
+                        Email = TxtEmail.Text,
+                        Password = TxtSenha.Text,
+                    }).Result;
+                    MessageBox.Show(usuario.Uid);
+                    MessageBox.Show(usuario.EmailVerified.ToString());
+                     Principal principal = new Principal();
+                     Hide();
+                     principal.Closed += fecharFormulario;
+                     principal.Show(); 
+
+                }
+                catch (Exception ex)
+                {
+                    if (ex.Message.Contains("The user with the provided email already exists (EMAIL_EXISTS"))
+                    {
+                        MessageBox.Show("Já existe o e-mail cadastrado");
+                    }
+                    else
+                    {
+
+                        MessageBox.Show("Erro:" + ex.Message);
+                       //  Clipboard.SetText(ex.Message); //
+                    }
+                }*/
+
             }
-            
         }
     }
 }
